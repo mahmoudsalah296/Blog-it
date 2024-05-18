89f58756bf41492579037d57c3b3612b0ba975e8
hamza-25 <me677499@gmail.com>
2024-05-18 20:22:22 +0100

    set connect db to blog_it database

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 5000;
@@ -9,6 +10,13 @@ app.get('/', (req, res) => {
     res.json({message: 'hello world'}).status(200);
 });
 
-app.listen(PORT, () =>{
-    console.log(`server running on port ${PORT}`);
+mongoose.connect(`mongodb://localhost:27017/blog_it`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () =>{
+        console.log(`server running on port ${PORT}`);
+    });
+}).catch((err) => {
+    console.error('Database connection error:', err);
 });
