cb28251cb033686d4f09c2bafcd28e0875d9eb15
hamza-25 <me677499@gmail.com>
2024-05-19 20:20:08 +0100

    add route create post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -12,7 +12,20 @@ route.get('/', async (req, res) => {
     }
 });
 
-
+// create a post 
+route.post('/', async (req, res) => {
+    const postData = req.body ? req.body : null;
+    if(!postData){
+        res.status(400).json({message: 'Something goes wrong'})
+    }
+    try{
+        const post = new Post(postData);
+        await post.save();
+        res.status(200).json({message: 'post created successfully'});
+    } catch (error){
+        res.status(400).json({error});
+    }
+});
 
 
 module.exports = route;
