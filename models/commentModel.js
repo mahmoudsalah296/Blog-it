7ffa085986feb6ee24d122307023010538731f3b
hamza-25 <me677499@gmail.com>
2024-05-27 16:28:38 +0100

    add two endpoint get comment by post and get post by category

--- models/commentModel.js
+++ models/commentModel.js
@@ -4,7 +4,6 @@ const commentSchema = new mongoose.Schema({
   body: { type: String, required: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-  // createdAt: { type: Date, default: Date.now },
 }, { timestamps: true });
 
 const Comment = mongoose.model('Comment', commentSchema, 'comments');
