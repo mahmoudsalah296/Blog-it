04e5d277fa3377d5c43cb140d2252ddffd8bf0e2
hamza-25 <me677499@gmail.com>
2024-05-29 15:33:05 +0100

    author to true

--- models/postModel.js
+++ models/postModel.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
   body: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: false },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
   //createdAt: { type: Date, default: Date.now },
   comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
