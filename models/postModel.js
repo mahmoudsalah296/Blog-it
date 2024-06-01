914681f36c173152f40bb6891815807c9b784b31
Mahmoud Salah <ms685116@gmail.com>
2024-06-01 23:38:49 +0300

    fix formatting

--- models/postModel.js
+++ models/postModel.js
@@ -1,16 +1,22 @@
 const mongoose = require('mongoose');
 
-
-const postSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  body: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
-  image: {type: String, require: false, default: "defaultImage.jpg"},
-  fileUrl: { type: String, required: false },
-  categoryName: {type: String, require: true},
-}, { timestamps: true });
+const postSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    categories: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Category' }],
+    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
+    image: { type: String, require: false, default: 'defaultImage.jpg' },
+    fileUrl: { type: String, required: false },
+    categoryName: { type: String, require: true },
+  },
+  { timestamps: true }
+);
 
 const Post = mongoose.model('Post', postSchema, 'post');
 
