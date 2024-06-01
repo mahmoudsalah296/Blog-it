8420d167e0e0d6ccab8ddce989c2dc8454067b89
Mahmoud Salah <ms685116@gmail.com>
2024-06-01 23:43:10 +0300

    fix formatting

--- middleware/verifyAdmin.js
+++ middleware/verifyAdmin.js
@@ -1,26 +1,26 @@
 const jwt = require('jsonwebtoken');
 
 const isAdmin = (req, res, next) => {
-    const authHeader = req.headers.authorization || req.headers.Authorization;
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
-    if (!authHeader?.startsWith("Bearer ")) {
-        return res.status(401).json({message: "Unauthorized"});
+  if (!authHeader?.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Forbidden' });
     }
 
-    const token = authHeader.split(" ")[1]
+    req.user = { id: decoded.UserInfo.id, isAdmin: decoded.UserInfo.isAdmin };
 
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, decoded) => {
-        if (err) {
-            return res.status(403).json({message: "Forbidden"})
-        }
-
-        req.user = {id: decoded.UserInfo.id, isAdmin: decoded.UserInfo.isAdmin};
-        
-        if (!decoded.UserInfo.isAdmin) {
-            return res.status(401).json({message: "unauthorized"})
-        }
-        next();
-    });
-}
+    if (!decoded.UserInfo.isAdmin) {
+      return res.status(401).json({ message: 'unauthorized' });
+    }
+    next();
+  });
+};
 
 module.exports = isAdmin;
--- middleware/verifyUserID.js
+++ middleware/verifyUserID.js
@@ -1,22 +1,21 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 const verifyUserID = (req, res, next) => {
-    const authHeader = req.headers.authorization || req.headers.Authorization;
+  const authHeader = req.headers.authorization || req.headers.Authorization;
 
-    if (!authHeader?.startsWith("Bearer ")) {
-        return res.status(401).json({message: "Unauthorized"});
+  if (!authHeader?.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Forbidden' });
     }
-
-    const token = authHeader.split(" ")[1]
-
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-        if (err) {
-            return res.status(403).json({message: "Forbidden"})
-        }
-        req.user= decoded.UserInfo.id;
-        next();
-    });
-
-}
+    req.user = decoded.UserInfo.id;
+    next();
+  });
+};
 
 module.exports = verifyUserID;
