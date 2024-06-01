df1ecc84c64d9133e2ba6ab324cb66a4a39580e3
Mahmoud Salah <ms685116@gmail.com>
2024-06-01 23:38:37 +0300

    fix formatting

--- models/commentModel.js
+++ models/commentModel.js
@@ -1,11 +1,18 @@
 const mongoose = require('mongoose');
 
-const commentSchema = new mongoose.Schema({
-  body: { type: String, required: true },
-  username: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
-}, { timestamps: true });
+const commentSchema = new mongoose.Schema(
+  {
+    body: { type: String, required: true },
+    username: { type: String, required: true },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    post: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
+  },
+  { timestamps: true }
+);
 
 const Comment = mongoose.model('Comment', commentSchema, 'comments');
 
