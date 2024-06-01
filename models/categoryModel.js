066925f488f39c13c7bddabb5dc4c7fe085e2701
Mahmoud Salah <ms685116@gmail.com>
2024-06-01 23:38:16 +0300

    fix formatting

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
-const categorySchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  description: { type: String , required: false, default: ''},
-}, { timestamps: true });
+const categorySchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    description: { type: String, required: false, default: '' },
+  },
+  { timestamps: true }
+);
 
 const Category = mongoose.model('Category', categorySchema, 'categories');
 
