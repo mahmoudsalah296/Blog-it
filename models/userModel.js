3dcd41c24fb6e1d04e5ea42f769dec1395b52cc6
Mahmoud Salah <ms685116@gmail.com>
2024-06-01 23:38:58 +0300

    fix formatting

--- models/userModel.js
+++ models/userModel.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: false },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  profilePicture: { type: String, required: false, default: 'defaultImage.jpg' },
-  bio: { type: String, required: false, default: '' },
-  isAdmin: {type: Boolean, default: false},
-}, { timestamps: true });
+const UserSchema = new mongoose.Schema(
+  {
+    username: { type: String, required: true, unique: false },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    profilePicture: {
+      type: String,
+      required: false,
+      default: 'defaultImage.jpg',
+    },
+    bio: { type: String, required: false, default: '' },
+    isAdmin: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model('User', UserSchema, 'users');
 module.exports = User;
